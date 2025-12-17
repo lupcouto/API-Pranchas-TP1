0d2c90ddd1365e96cc1d93dbb94b55e42038215d
Luisa Pimentel <lupcouto.2006@gmail.com>
2025-12-17 08:32:11 -0300

    model

---
+++ Usuario.java
@@ -0,0 +1,36 @@
+package br.unitins.topicos1.prancha.model;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+
+@Entity
+public class Usuario extends DefaultEntity {
+
+    private String nome;
+
+    @Column(unique = true)
+    private String login;
+    private String senha;
+    
+    private Perfil perfil;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public Perfil getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(Perfil perfil) {
+        this.perfil = perfil;
+    }
+
+}
